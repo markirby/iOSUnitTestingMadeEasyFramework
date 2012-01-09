0897fb00b627c2455bf2840d1ee0d6d58322c76f
Mark Kirby <markirby@Marks-MacBook-Air-2.local>
2012-01-09 17:55:29 +0000

    Adding framework base files

---
+++ iOSUnitTestingMadeEasyFramework/iOSUnitTestingMadeEasyFramework.h
@@ -0,0 +1,13 @@
+//
+//  iOSUnitTestingMadeEasyFramework.h
+//  iOSUnitTestingMadeEasyFramework
+//
+//  Created by Mark Kirby on 09/01/2012.
+//  Copyright (c) 2012 Hatch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSUnitTestingMadeEasyFramework : NSObject
+
+@end
---
+++ iOSUnitTestingMadeEasyFramework/iOSUnitTestingMadeEasyFramework.m
@@ -0,0 +1,23 @@
+//
+//  iOSUnitTestingMadeEasyFramework.m
+//  iOSUnitTestingMadeEasyFramework
+//
+//  Created by Mark Kirby on 09/01/2012.
+//  Copyright (c) 2012 Hatch. All rights reserved.
+//
+
+#import "iOSUnitTestingMadeEasyFramework.h"
+
+@implementation iOSUnitTestingMadeEasyFramework
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
