747abdde005dfcd69a2b3230c2899337f850c15c
Mark Kirby <markirby@Marks-MacBook-Air-2.local>
2012-01-09 20:04:38 +0000

    Added TestsNib protocol

---
+++ iOSUnitTestingMadeEasyFramework/TestsNib.h
@@ -0,0 +1,13 @@
+//
+//  TestsNib.h
+//  iOSUnitTestingMadeEasyFramework
+//
+//  Created by Mark Kirby on 09/01/2012.
+//  Copyright (c) 2012 Hatch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol TestsNib <NSObject>
+
+@end
